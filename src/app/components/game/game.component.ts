0fbd16e3bd02aee5f3917179e06c5020061ee507
Jorgito Ramírez Galvan <jorgeramirezgalvan99@gmail.com>
2024-02-17 19:05:24 +0100

    created start panel component and styles

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TypeQuoteComponent } from './components/type-quote/type-quote.component';
+import { StartPanelComponent } from './components/start-panel/start-panel.component';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [TypeQuoteComponent],
+  imports: [TypeQuoteComponent, StartPanelComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
