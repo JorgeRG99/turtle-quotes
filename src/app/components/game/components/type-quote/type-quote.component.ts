0bba118aae4a9da20e0fb8ff73f19104d8cb342b
Jorgito Ramírez Galvan <jorgeramirezgalvan99@gmail.com>
2024-02-17 18:25:00 +0100

    completed game service to handle type logic

--- src/app/components/game/components/type-quote/type-quote.component.ts
+++ src/app/components/game/components/type-quote/type-quote.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GameService } from '../../../../services/game.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-type-quote',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './type-quote.component.html',
-  styleUrl: './type-quote.component.scss'
+  styleUrl: './type-quote.component.scss',
 })
 export class TypeQuoteComponent {
+  gameService = inject(GameService);
+  initialQuote$!: Observable<string>;
+  rightQuote$!: Observable<string>;
+  currentCharacter$!: Observable<string>;
+  isCurrentCharacterWrong$!: Observable<boolean>;
 
+  ngOnInit() {
+    this.initialQuote$ = this.gameService.getQuote();
+    this.rightQuote$ = this.gameService.getRightPart();
+    this.currentCharacter$ = this.gameService.getCurrentChar();
+    this.isCurrentCharacterWrong$ = this.gameService.getIsCurrentCharWrong();
+  }
 }
---
+++ src/app/services/game.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, fromEvent, map } from 'rxjs';
+import { ApiResultObject } from '../models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GameService {
+  private apiUrl: string = environment.apiBaseUrl;
+  private quote = new BehaviorSubject('');
+  private right = new BehaviorSubject('');
+  private currentChar = new BehaviorSubject('');
+  private isCurrentCharWrong = new BehaviorSubject(false);
+  private keydown = fromEvent<KeyboardEvent>(document, 'keydown');
+
+  constructor(private httpClient: HttpClient) {
+    this.requestQuote();
+
+    this.keydown.subscribe((e) => {
+      if (e.key === this.currentChar.getValue()) {
+        this.handleCorrectInput();
+      } else {
+        this.setIsCurrentCharWrong(true);
+      }
+    });
+  }
+
+  requestQuote(): void {
+    this.httpClient
+      .get<ApiResultObject[]>(this.apiUrl)
+      .pipe(map((result) => result[0].content))
+      .subscribe((quoteContent) => {
+        this.setQuote(quoteContent.slice(1));
+        this.setCurrentChar(quoteContent[0]);
+      });
+  }
+
+  handleCorrectInput() {
+    this.setRightPart(this.right.value + this.currentChar.value);
+    this.setCurrentChar(this.quote.value[0]);
+    this.setQuote(this.quote.value.slice(1));
+
+    if (this.isCurrentCharWrong.value) this.setIsCurrentCharWrong(false);
+  }
+
+  // GETTERS
+  getQuote(): Observable<string> {
+    return this.quote.asObservable();
+  }
+
+  getRightPart(): Observable<string> {
+    return this.right.asObservable();
+  }
+
+  getCurrentChar(): Observable<string> {
+    return this.currentChar.asObservable();
+  }
+
+  getIsCurrentCharWrong(): Observable<boolean> {
+    return this.isCurrentCharWrong.asObservable();
+  }
+
+  // SETTERS
+  setQuote(value: string): void {
+    this.quote.next(value);
+  }
+
+  setRightPart(value: string): void {
+    this.right.next(value);
+  }
+
+  setCurrentChar(value: string): void {
+    this.currentChar.next(value);
+  }
+
+  setIsCurrentCharWrong(value: boolean): void {
+    this.isCurrentCharWrong.next(value);
+  }
+}
