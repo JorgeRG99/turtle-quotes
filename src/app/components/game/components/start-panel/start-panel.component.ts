a445e99004118b545aca713df426855e6a1fb877
Jorgito Ramírez Galvan <jorgeramirezgalvan99@gmail.com>
2024-02-17 19:04:17 +0100

    created start panel component and styles

---
+++ src/app/components/game/components/start-panel/start-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './start-panel.component.html',
+  styleUrl: './start-panel.component.scss'
+})
+export class StartPanelComponent {
+
+}
